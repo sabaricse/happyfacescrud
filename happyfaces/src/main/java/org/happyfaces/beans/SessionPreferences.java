36b8ec595369ba3d3ac77711d4b6921335c4817e
ignas.lelys <ignas.lelys@7dd97c7d-6ec1-23af-e099-a2056184613a>
2013-02-26 22:00:41 +0000

    Tickt #5, rewriting to jsf 2 composite components style. Without bugfixes yet.

--- happyfaces/src/main/java/org/happyfaces/beans/SessionPreferences.java
+++ happyfaces/src/main/java/org/happyfaces/beans/SessionPreferences.java
@@ -10,33 +10,70 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 
+/**
+ * Static and non static methods for session related information and actions.
+ * 
+ * @author Ignas
+ *
+ */
 @ManagedBean(name = "sessionPreferences")
 @SessionScoped
 public class SessionPreferences implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    
+    private Locale locale;
+    
+    public SessionPreferences() {
+    	this.locale = new Locale("ru");
+    	FacesContext.getCurrentInstance().getViewRoot().setLocale(this.locale);
+    }
 
-    private Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+    public static Locale getCurrentLocale() {
+        return FacesContext.getCurrentInstance().getViewRoot().getLocale();
+    }
     
     public Locale getLocale() {
+        if (locale == null) {
+            changeLocale(SessionPreferences.getCurrentLocale().getLanguage());
+        }
         return locale;
     }
 
-    /**
-     * Changes locale for user.
-     */
     public void changeLocale(String language) {
         locale = new Locale(language);
         FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
+    
+    /**
+     * Check if user has authority to edit. Override if specific role is
+     * required for different pages.
+     * 
+     * @return true if edit is allowed
+     */
+    public boolean isEditAllowed() {
+        return hasRole("ROLE_USER");
     }
 
     /**
      * Check if user has role.
      */
-    public static final boolean hasRole(String role) {
+    private boolean hasRole(String role) {
         HttpServletRequest req = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
         SecurityContextHolderAwareRequestWrapper sc = new SecurityContextHolderAwareRequestWrapper(req, "");
         return sc.isUserInRole(role);
     }
 
+    /**
+    * Return authenticated user name.
+//    */
+//   public static String getUserName() {
+//       try {
+//           UserDetails authenticatedUser = (UserDetails) ((SecurityContext) SecurityContextHolder.getContext()).getAuthentication().getPrincipal();
+//           return authenticatedUser.getUsername();
+//       } catch (Throwable e) {
+//           return "authentication error";
+//       }
+//   }
+    
 }
---
+++ happyfaces/src/main/java/org/happyfaces/jsf/composite/FormEntityFieldCompositeComponent.java
@@ -0,0 +1,14 @@
+package org.happyfaces.jsf.composite;
+
+import javax.faces.component.FacesComponent;
+
+/**
+ * Backing UINamingContainer for searchField.xhtml composite component.
+ * 
+ * @author Ignas
+ * 
+ */
+@FacesComponent(value = "formEntityField")
+public class FormEntityFieldCompositeComponent extends BackingBeanBasedCompositeComponent {
+
+}
