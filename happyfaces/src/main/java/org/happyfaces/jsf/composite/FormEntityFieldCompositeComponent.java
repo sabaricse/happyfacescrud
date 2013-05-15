c26225bcba42edac9f86e06bc1c300ef05409541
ignas.lelys <ignas.lelys@7dd97c7d-6ec1-23af-e099-a2056184613a>
2013-05-15 20:15:37 +0000

    Fix edit for formEntityField component

--- happyfaces/src/main/java/org/happyfaces/jsf/composite/FormEntityFieldCompositeComponent.java
+++ happyfaces/src/main/java/org/happyfaces/jsf/composite/FormEntityFieldCompositeComponent.java
@@ -9,6 +9,6 @@ import javax.faces.component.FacesComponent;
  * 
  */
 @FacesComponent(value = "formEntityField")
-public class FormEntityFieldCompositeComponent extends BaseBeanBasedCompositeComponent {
+public class FormEntityFieldCompositeComponent extends FormFieldCompositeComponent {
 
 }
